ce07cd8eab2d49bb426ce0ed50252e49f801a211
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-03-14 15:35:10 +0700

    script to bvi has been created

--- main.js
+++ main.js
@@ -4,9 +4,32 @@ import {Mask, MaskInput} from "maska"
 import './sass/app.scss';
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
+import * as isvek from "bvi";
+import 'bvi/dist/css/bvi.min.css';
+import Swiper from "swiper";
+
 new MaskInput("[data-maska]")
 
 //fancybox (может конфликтовать)
 Fancybox.bind("[data-fancybox]", {
     // Your custom options
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("js-header-bvi").addEventListener("click", function () {
+        if (typeof isvek !== "undefined" && typeof isvek.Bvi === "function") {
+            new isvek.Bvi({
+                target: "body",
+                theme: "white",
+                font: "arial",
+                letterSpacing: "normal",
+                lineHeight: "normal",
+                images: true,
+                reload: false
+            });
+            console.log("BVI успешно запущен!");
+        } else {
+            console.error("Ошибка: isvek.Bvi не найден. Проверьте подключение скрипта.");
+        }
+    });
 });
