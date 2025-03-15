3512d08beb31818896d25239eba34e12e4d8280e
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-03-15 18:02:25 +0700

    news index page has been created

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                newsIndex: resolve(__dirname, '/pages/_newsIndex.html'),
             },
         },
     },
